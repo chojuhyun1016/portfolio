c82ff23ac28b28d39e3b41cb33c8a61219044cbb
Cho Juhyun <chojuhyun1016@gmail.com>
2025-04-14 18:09:26 +0900

    2025/04/14 - 키 교체 전략 반영   -> LockKeyGeneratorFactory   -> 테스트 기능 추가

--- order-core/src/main/java/org/example/order/core/lock/annotation/DistributedLock.java
+++ order-core/src/main/java/org/example/order/core/lock/annotation/DistributedLock.java
@@ -8,7 +8,7 @@ import java.lang.annotation.*;
 public @interface DistributedLock {
     String key();
     String type(); // namedLock, redissonLock
-    String keyStrategy() default "spel"; // ✅ 추가: 키 전략
+    String keyStrategy() default "spell"; // ✅ 추가: 키 전략
     long waitTime() default 5000;
     long leaseTime() default 10000;
 }
