d2155ed4da490f2da02cd2e25107f8546bca28c6
Cho Juhyun <chojuhyun1016@gmail.com>
2025-05-17 18:37:34 +0900

    2025/05/17 - order-worker 활성화   -> 신규 구조로 커스터 마이징   -> 추가 작업 필요      dto, entity, vo 등의 네이밍 체계화
    - 버그 수정
      order-client:web
    
    to-do: - order-worker
             코드 추가 개선
             문서화 작업
           - application.yml 정리 (김진욱)
           - 테스트 코드 수정 (김진욱)
             내부 컨테이너, H2DB 등을 이용하도록 수정
           - order-worder 개선
           - order-batch 개선
           - order-api 개선

---
+++ order-client/web/src/main/java/org/example/order/client/web/config/property/WebClientUrlProperties.java
@@ -0,0 +1,30 @@
+package org.example.order.client.web.config.property;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@ConfigurationProperties("web-client")
+public class WebClientUrlProperties {
+    private Client client;
+
+    @Getter
+    @Setter
+    public static class Client {
+        private String clientId;
+        private Url url;
+    }
+
+    @Getter
+    @Setter
+    public static class Url {
+        String order;
+        String user;
+
+        public String getWithPathVariable(String url, Object path) {
+            return String.format("%s/%s", url, path);
+        }
+    }
+}
