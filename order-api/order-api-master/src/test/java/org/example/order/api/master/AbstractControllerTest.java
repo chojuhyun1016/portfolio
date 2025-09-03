2b41fedcaee5a74d24ac1359fe3f329e2fac54c2
r00365 <juhyun.cho@riman.com>
2025-09-03 14:48:55 +0900

    2025/09/3 - order-api 시작   build 환경 구성

--- order-api/order-api-master/src/test/java/org/example/order/api/master/AbstractControllerTest.java
+++ order-api/order-api-master/src/test/java/org/example/order/api/master/AbstractControllerTest.java
@@ -1,29 +1,29 @@
-package org.example.order.api.master;
-
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-
-@ExtendWith({SpringExtension.class, RestDocumentationExtension.class})
-@AutoConfigureMockMvc
-@AutoConfigureRestDocs(
-        outputDir = "build/generated-snippets",
-        uriScheme = "https",
-        uriHost = "api.example.com",
-        uriPort = 443
-)
-@TestPropertySource(properties = {
-        "order.api.infra.security.enabled=false",
-        "order.api.infra.logging.enabled=false",
-        "order.api.infra.web.enabled=false",
-        "order.api.infra.format.enabled=false"
-})
-public abstract class AbstractControllerTest {
-    @Autowired
-    protected MockMvc mockMvc;
-}
+//package org.example.order.api.master;
+//
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.restdocs.RestDocumentationExtension;
+//import org.springframework.test.context.TestPropertySource;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
+//import org.springframework.test.web.servlet.MockMvc;
+//
+//@ExtendWith({SpringExtension.class, RestDocumentationExtension.class})
+//@AutoConfigureMockMvc
+//@AutoConfigureRestDocs(
+//        outputDir = "build/generated-snippets",
+//        uriScheme = "https",
+//        uriHost = "api.example.com",
+//        uriPort = 443
+//)
+//@TestPropertySource(properties = {
+//        "order.api.infra.security.enabled=false",
+//        "order.api.infra.logging.enabled=false",
+//        "order.api.infra.web.enabled=false",
+//        "order.api.infra.format.enabled=false"
+//})
+//public abstract class AbstractControllerTest {
+//    @Autowired
+//    protected MockMvc mockMvc;
+//}
--- order-api/order-api-master/src/test/java/org/example/order/api/master/utils/ApiDocumentUtils.java
+++ order-api/order-api-master/src/test/java/org/example/order/api/master/utils/ApiDocumentUtils.java
@@ -1,21 +1,21 @@
-package org.example.order.api.master.utils;
-
-import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
-import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
-
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
-
-public final class ApiDocumentUtils {
-    private ApiDocumentUtils() {}
-
-    public static OperationRequestPreprocessor getDocumentRequest() {
-        return preprocessRequest(
-                modifyHeaders().remove("X-Internal-Auth"),
-                prettyPrint()
-        );
-    }
-
-    public static OperationResponsePreprocessor getDocumentResponse() {
-        return preprocessResponse(prettyPrint());
-    }
-}
+//package org.example.order.api.master.utils;
+//
+//import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
+//import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
+//
+//import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+//
+//public final class ApiDocumentUtils {
+//    private ApiDocumentUtils() {}
+//
+//    public static OperationRequestPreprocessor getDocumentRequest() {
+//        return preprocessRequest(
+//                modifyHeaders().remove("X-Internal-Auth"),
+//                prettyPrint()
+//        );
+//    }
+//
+//    public static OperationResponsePreprocessor getDocumentResponse() {
+//        return preprocessResponse(prettyPrint());
+//    }
+//}
